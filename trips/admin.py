bd3fcd44595fc2b5f5db1d53fc61a151f875900e
chief <chief@localhost.localdomain>
2022-05-19 13:32:57 -0700

    trips page

--- trips/admin.py
+++ trips/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import River
 # Register your models here.
+
+class RiverAdmin(admin.ModelAdmin):
+	list_display = ('name', 'location', 'itinerary', 'duration', 'difficulty')
+
+admin.site.register(River, RiverAdmin)
