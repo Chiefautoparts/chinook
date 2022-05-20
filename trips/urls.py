fa2958ae00ebec5512fc670481040df737bfc3a8
chief <chief@localhost.localdomain>
2022-05-20 04:48:02 -0700

    stuff

--- trips/urls.py
+++ trips/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-	
+	path('rivers/', views.RiverListView.as_view(), name='rivers'),
+	path('river/<int:ok>', views.RiverDetailView.as_view(), name='river-detail'),
 ]
--- trips/views.py
+++ trips/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from django.views import generic
 from .models import River
 
 # Create your views here.
+class RiverListView(generic.DetailView):
+	model = River 
+	def river_detail_view(request, primary_key):
+		try:
+			river = River.objects.get(pl=primary_key)
+		except River.DoesNotExist:
+			raise Http404('No Such River')
+
+		return render(request, 'river/river_detail.html', context={'river': river})
+
 def index(request):
 	return render(request, 'rivers.html')
 
