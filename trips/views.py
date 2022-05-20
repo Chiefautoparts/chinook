6c1aa74a2433a65858a67a1d8abd89f303470c30
chief <chief@localhost.localdomain>
2022-05-20 03:30:50 -0700

    added gitignore and started views.py for trips

--- trips/views.py
+++ trips/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import River
 
 # Create your views here.
+def index(request):
+	return render(request, 'rivers.html')
+
+def displayRivers(request):
+	rivers = River.objects.all()
