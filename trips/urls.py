06edd6e1dac1090714b429cfb321801210c27f67
chief <chief@localhost.localdomain>
2022-05-26 13:50:16 -0700

    minor changes, about to change branches, first commit of the day

--- trips/urls.py
+++ trips/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 urlpatterns = [
 	path('rivers/', views.RiverListView.as_view(), name='rivers'),
-	path('river/<int:ok>', views.RiverDetailView.as_view(), name='river-detail'),
+	path('river/<int:ok>', views.RiverListView.as_view(), name='river-detail'),
 ]
